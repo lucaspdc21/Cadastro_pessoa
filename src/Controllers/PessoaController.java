b251e373501dc13a01b9f6922dd8cb249659ecb1
Luiza Pessoa <esthermartins@alu.ufc.br>
2024-08-13 09:53:11 -0300

    Add Delete Method

--- src/Controllers/PessoaController.java
+++ src/Controllers/PessoaController.java
@@ -11,17 +11,39 @@ import Models.Pessoa;
 
 public class PessoaController {
 
-    private String enderecoArquivo = "pessoas.txt";
+    private String enderecoArquivo = "pessoas.txt"; 
     private List<Pessoa> listaDePessoas = new ArrayList<>();
 
+    // Construtor que chama a função lerArquivo e armazena os dados em uma lista
     public PessoaController() {
         lerArquivo();
     }
 
+    // Adiciona o usuário na lista: objeto "Pessoa" na lista "Pessoas"
     public void cadastrarPessoa(Pessoa pessoa) {
         listaDePessoas.add(pessoa);
     }
 
+    // Remove a pessoa pelo nome e atualiza o arquivo
+    public void deletarPessoa(String nome) {
+        Pessoa pessoaParaRemover = null;
+        for (Pessoa pessoa : listaDePessoas) {
+            if (pessoa.getNome().equalsIgnoreCase(nome)) {
+                pessoaParaRemover = pessoa;
+                break;
+            }
+        }
+
+        if (pessoaParaRemover != null) {
+            listaDePessoas.remove(pessoaParaRemover);
+            System.out.println("Pessoa " + nome + " removida com sucesso.");
+        } else {
+            System.out.println("Pessoa " + nome + " não encontrada.");
+            return;
+        }
+    }
+
+    // Função que lê linha por linha do arquivo pessoas.txt
     private void lerArquivo() {
         try (FileReader fr = new FileReader(enderecoArquivo);BufferedReader br = new BufferedReader(fr)) {
             String linhaLida;
@@ -34,18 +56,20 @@ public class PessoaController {
         }
     }
 
+    // Armazena os dados do usuário após a leitura correta das informações
     private void escreverArquivo() {
         try (FileWriter fw = new FileWriter(enderecoArquivo, false);BufferedWriter bw = new BufferedWriter(fw)) {
             for (Pessoa pessoa : listaDePessoas) {
                 bw.write(pessoa.toString());
                 bw.newLine();
             }
-            bw.flush();
+            bw.flush(); // Libera o buffer
         } catch (IOException e) {
             System.out.println("Erro ao escrever no arquivo de pessoas");
         }
     }
 
+    // Encerra o programa após escrever os dados no arquivo
     public void encerrarPrograma() {
         escreverArquivo();
     }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         Pessoa pessoa = new Pessoa("Lucas", 21, "example@teste.com", "(85)9999-9999");
         PessoaController pc = new PessoaController();
         pc.cadastrarPessoa(pessoa);
-        pc.fecharArquivo();
+        pc.deletarPessoa("Joao");
+        pc.encerrarPrograma();
     }
 }
